e8b9785d299944f15e79b1d4fd5387f4073b8970
Unknown <esquinca_jose@hotmail.com>
2018-01-19 17:20:38 -0500

    redireccion a ibero
    se agrega redir a pagina de ibero si pasan 5 secs

--- redir.php
+++ redir.php
@@ -37,12 +37,61 @@
 				return ruta;
 			}
 
+			function cronometro() {
+					 var cronometro;
+					 var contador_s =0;
+					 var contador_m =0;
+					 var s = document.getElementById("segundos");
+					 var m = document.getElementById("minutos");
+					 cronometro = setInterval(
+								function(){
+										if(contador_s==60)
+										{
+												contador_s=0;
+												contador_m++;
+												console.log(contador_m);
+												document.getElementById('minutos').innerHTML = contador_m;
+											 //  m.innerHTML = contador_m;
+												if(contador_m==60)
+												{
+														contador_m=0;
+												}
+										}
+										document.getElementById('segundos').innerHTML = contador_s;
+										console.log(contador_s);
+									 //  s.innerHTML = contador_s;
+										contador_s++;
+								}
+								,1000);
+			}
+
+
+			function cronometro_inverso() {
+					 var cronometro;
+					 var contador_s =5;
+					 var redirnormal = "https://www.iberostar.com/en";
+					 cronometro = setInterval(
+					 function(){
+								if(contador_s==0)
+								{
+									document.getElementById('segundos').innerHTML = 0;
+									window.location.replace(redirnormal);
+									console.log('detener');
+								}
+								else {
+									document.getElementById('segundos').innerHTML = contador_s;
+									console.log(contador_s);
+									contador_s--;
+								}
+					 },1000);
+			}
+
 			(function () {
 				var MOBILE_SITE = '/mobile/index.html', // site to redirect to
 				NO_REDIRECT = 'noredirect'; // cookie to prevent redirect
 				// I only want to redirect iPhones, Android phones and a handful of 7" devices
-
 				// Mover a página de redirección.
+				cronometro_inverso();
 				if (isMobile.apple.device || isMobile.android.phone || isMobile.seven_inch) {
 
 					if (isMobile.apple.device){
@@ -61,6 +110,7 @@
 					// to explicitly view the full site. This is validated
 					// by checking if a "noredirect" cookie exists
 					if ( document.cookie.indexOf(NO_REDIRECT) === -1 ) {
+
 						document.location = fallbackLink;
 						//window.location.replace(fallbackLink);
 					}
@@ -73,7 +123,7 @@
 
 
 	<body>
-
+	<span style="font-family: Courier; display:none;" id="segundos"></span>
 		<div class="site-content">
 			<main class="main-content">
 				<div class="site-footer">
